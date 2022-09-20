ab661d9937a82ba3c1c4eca30a51a58eee75686b
user19 <user19@clipcoupon.com>
2022-09-20 16:42:28 -0400

    Added Unit and integration Tests

---
+++ src/main/java/com/mcsv/hello/hello1/service/Hello1Service.java
@@ -0,0 +1,7 @@
+package com.mcsv.hello.hello1.service;
+
+public class Hello1Service {
+    public String getWelcomeMessage(String name){
+        return String.format("Welcome %s!", name);
+    }
+}
